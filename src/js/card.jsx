7e7ae90b44e4bd588f9017cdcd0acbd47a8c06e3
Shivangi Tikekar <shivz@live.in>
2019-01-04 15:42:13 +0530

    loading screen added

--- src/js/card.jsx
+++ src/js/card.jsx
@@ -1,28 +1,33 @@
-import React from 'react';
-import axios from 'axios';
-import ta from 'time-ago';
+import React from "react";
+import axios from "axios";
+import ta from "time-ago";
 import DCard from "./DCard.js";
+import VisibilitySensor from "react-visibility-sensor";
 // import { parse as parseURL } from 'url';
 
 export default class toDoctorCard extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     let stateVar = {
       level: 0,
       data: {},
       fetchingData: false,
-      searchTerm: null,
+      searchTerm: "Shiv",
       searchPlaceholder: "Search",
-      domain: undefined
+      domain: undefined,
+      visible: null,
+      checkVis: false
     };
 
     if (this.props.dataJSON) {
       stateVar.fetchingData = false;
       stateVar.dataJSON = this.props.dataJSON;
-      stateVar.languageTexts = this.getLanguageTexts(this.props.dataJSON.data.language);
+      stateVar.languageTexts = this.getLanguageTexts(
+        this.props.dataJSON.data.language
+      );
     }
 
-    if(this.props.domain){
+    if (this.props.domain) {
       stateVar.domain = this.props.domain;
     }
 
@@ -32,6 +37,7 @@ export default class toDoctorCard extends React.Component {
 
     this.state = stateVar;
     this.goHome = this.goHome.bind(this);
+    this.checkVis = this.visOnChange.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -40,45 +46,58 @@ export default class toDoctorCard extends React.Component {
     this.setState({
       [target.name]: target.value
     });
-    console.log(this.state)
+    console.log(this.state);
   }
 
   goHome() {
     this.setState({
       level: 0,
       searchPlaceholder: "Search name"
-    })
+    });
+  }
+
+  visOnChange() {
+    console.log(window.scrollX);
   }
 
   handleSubmit() {
-    let API = 'https://search-dodgy-doctors-izsk7ozifcsawmvchoxsv3p43u.ap-south-1.es.amazonaws.com/doctors/_search'
+    this.setState({ level: 1 });
+    let API =
+      "https://search-dodgy-doctors-izsk7ozifcsawmvchoxsv3p43u.ap-south-1.es.amazonaws.com/doctors/_search";
     let DEFAULT_QUERY = {
-    "query": {
-        "bool" : {
-            "must" : {
-                "query_string" : {
-                    "query" : this.state.searchTerm // User Provided Name
-                }
-            },
-            "should": {
-                "term": {"country": "India"}
+      query: {
+        bool: {
+          must: {
+            query_string: {
+              query: this.state.searchTerm // User Provided Name
             }
+          },
+          should: {
+            term: { country: "India" }
+          }
         }
-    }
-}
-    axios.get(API, {
-  params: {
-    source: JSON.stringify(DEFAULT_QUERY),
-    source_content_type: 'application/json'
-  }
-})
+      }
+    };
+    axios
+      .get(API, {
+        params: {
+          source: JSON.stringify(DEFAULT_QUERY),
+          source_content_type: "application/json"
+        }
+      })
       .then(result => {
-        this.setState({
-          data: result.data.hits.hits,
-          level: 1, searchPlaceholder: "Next search" 
-        })
-        console.log(result.data.hits.hits)
-    })
+        setTimeout(
+          function() {
+            this.setState({
+              data: result.data.hits.hits,
+              level: 2,
+              searchPlaceholder: "Next search"
+            });
+          }.bind(this),
+          2000
+        );
+        console.log(result.data.hits.hits);
+      });
   }
 
   exportData() {
@@ -93,76 +112,102 @@ export default class toDoctorCard extends React.Component {
       let data = this.state.data;
       return (
         <div className="pro-col-4">
-        <div className="container">
-          <div className="title" onClick={this.goHome}>FARZI DOCTOR</div>
-           <div className="result-cards">
-            {this.state.level === 0 && (
-            <div className="intro-card">
-              <div className="content">
-                <div className="header">
-                  <div className="header__title">
-                    Is Your Doctor Registered?
-                  </div>
-                  <div className="header__icon">
-                  <img src="Doc.svg" width="56px" />
-                  </div>
-                </div>
-                <div className="description">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </div>
-                <div className="expand">Read more</div>
-              </div>
+          <div className="container">
+            <div className="title" onClick={this.goHome}>
+              FARZI DOCTOR
             </div>
-          )}
-              {this.state.level === 1 && data.length > 0 && (data.map(e => (
-                <DCard data={e._source} count={count} />
-              )))}
-              {this.state.level === 1 && data.length === 0 && (
+            <div className="result-cards" id="result-cards">
+              {this.state.level === 1 && (
                 <div className="intro-card">
-                <div className="content">
-                No results found.</div></div>
+                  <div className="content">
+                    <div className="loading-container">
+                      <div className="loading-text">Checking records for</div>
+                      <div className="loading-name">
+                        {this.state.searchTerm}
+                      </div>
+                      <div className="label">Searching</div>
+                      <div className="progress" />
+                    </div>
+                  </div>
+                </div>
+              )}
+              {this.state.level === 0 && (
+                <div className="intro-card">
+                  <div className="content">
+                    <div className="header">
+                      <div className="header__title">
+                        Is Your Doctor Registered?
+                      </div>
+                      <div className="header__icon">
+                        <img src="Doc.svg" width="56px" />
+                      </div>
+                    </div>
+                    <div className="description">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                      ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </div>
+                    <div className="expand">Read more</div>
+                  </div>
+                </div>
+              )}
+              {this.state.level === 2 &&
+                data.length > 0 &&
+                data.map((e, i) => (
+                  <VisibilitySensor
+                    onChange={this.visOnChange}
+                    containment={document.getElementById("result-cards")}
+                  >
+                    <DCard data={e._source} count={count} />
+                  </VisibilitySensor>
+                ))}
+              {this.state.level === 2 &&
+                data.length === 0 && (
+                  <div className="intro-card">
+                    <div className="content">No results found.</div>
+                  </div>
                 )}
             </div>
+            <div className="scroll-container">
+              {this.state.level === 2 &&
+                data.length > 0 &&
+                data.map(e => (
+                  <div className="scroll-circle" key={e.registration_number} />
+                ))}
+            </div>
             <div className="search-container">
-              <input className="search-input" key={this.state.level} name="searchTerm" placeholder={this.state.searchPlaceholder} onChange={this.handleChange} onFocus={this.value = ''}></input>
-              <div className="search-submit" onClick={this.handleSubmit}><img src="arrow-right.svg" width="16px" className="search-submit-arrow" /></div>
+              <input
+                className="search-input"
+                key={this.state.level}
+                name="searchTerm"
+                placeholder={this.state.searchPlaceholder}
+                onChange={this.handleChange}
+                onFocus={(this.value = "")}
+              />
+              <div className="search-submit" onClick={this.handleSubmit}>
+                <img
+                  src="arrow-right.svg"
+                  width="16px"
+                  className="search-submit-arrow"
+                />
+              </div>
             </div>
           </div>
         </div>
       );
     }
   }
-  
-  getLanguageTexts(languageConfig) {
-    let language = languageConfig ? languageConfig : "hindi",
-      text_obj;
 
-    switch(language.toLowerCase()) {
-      case "hindi":
-        text_obj = {
-          font: "'Sarala', sans-serif"
-        }
-        break;
-      default:
-        text_obj = {
-          font: undefined
-        }
-        break;
-    }
-    return text_obj;
-  }
   render() {
-    switch(this.props.mode) {
-      case 'col16':
+    switch (this.props.mode) {
+      case "col16":
         return this.renderSixteenCol();
-      case 'col7':
+      case "col7":
         return this.renderSevenCol();
-      case 'col4':
+      case "col4":
         return this.renderFourCol();
-      case 'col2':
+      case "col2":
         return this.renderTwoCol();
       default:
         return this.renderHTML(this.state.dataJSON.data);
