865a6b3dd7cc325979f3e5574a29d6f72c6444b9
Shivangi Tikekar <shivz@live.in>
2019-01-09 13:41:05 +0530

    load more implemented

--- src/js/DComponent.js
+++ src/js/DComponent.js
@@ -8,17 +8,18 @@ class DComponent extends Component {
 		super();
 
 		this.state = {
-			visible: null
+			visible: null,
+			showMore: false
 		}
 
-		this.loadFunc = this.loadFunc.bind(this);
+		this.renderCards = this.renderCards.bind(this);
+		this.loadMore = this.loadMore.bind(this);
 	}
 
 	visChange(x,i) {
 		this.setState({
-			visible: i
+			visible: i%10
 		})
-		console.log(x,i)
 	}
 
 	componentDidRender() {
@@ -27,42 +28,36 @@ class DComponent extends Component {
 		})
 	}
 
-	loadFunc() {
-		console.log("triggered")
+	loadMore(data) {
+		this.setState({showMore: true})
+	}
+
+	renderCards(data, count) {
+		return (
+			data.map((e, i) => (
+							<VisibilitySensor onChange={x => this.visChange(x,i)} containment={document.getElementById("result-cards")}>
+							<DCard data={e._source} count={count} />
+							</VisibilitySensor>
+						)))		
 	}
 
 	render() {
-		let data = this.props.data.slice(0, 10);
+		let data = this.props.data;
 		let count = this.props.count;
 		return (
 			<div>
 				<div className="result-cards" id="result-cards" ref={(ref) => this.scrollParentRef = ref}>
-				<InfiniteScroll
-		            pageStart={0}
-		            loadMore={this.loadFunc}
-		            hasMore={true}
-		            loader={<div className="loader" key={0}>Loading ...</div>}
-		            useWindow={false}
-		            getScrollParent={() => this.scrollParentRef}
-		        >
-					{data.length > 0 &&
-						data.map((e, i) => (
-							<VisibilitySensor onChange={x => this.visChange(x,i)} containment={document.getElementById("result-cards")}>
-							<DCard data={e._source} count={count} />
-							</VisibilitySensor>
-						))}
-				</InfiniteScroll>
+					{this.renderCards(data.slice(0, 10), count)}
+					{this.state.showMore ? (this.renderCards(data.slice(10), count)) : (<div className="card" onClick={() => this.loadMore(data, count)}>Load more</div>)}
 				</div>
-
 				<div className="scroll-container">
-					{data &&
-						data.map((e, i) => (
-							<div
+				{[...Array(10)].map((x, i) =>
+    						<div
 								className="scroll-circle"
 								id={i}
 								style={{backgroundColor: (i === this.state.visible ? 'grey' : 'lightgrey')}}
 							/>
-						))}
+  						)}
 				</div>
 			</div>
 		);
